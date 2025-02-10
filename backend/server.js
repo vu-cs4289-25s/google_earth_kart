4cab2c3ef273e4b43805b5c3fe0e708f9c22d393
jacfrist <jacfrist@gmail.com>
2025-02-10 13:37:14 -0600

    fixed websockets to integrate w react

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,26 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server); // For websocket functions
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true
+}));
+
+const io = new Server(server, { // Allow websockets to connect
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Access-Control-Allow-Origin"],
+        credentials: true
+    }
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
