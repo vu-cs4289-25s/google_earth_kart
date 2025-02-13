d43e30984523d4f9d3fbb28e3713a29faaf9463d
jacfrist <jacfrist@gmail.com>
2025-02-12 20:10:02 -0600

    ok wait we getting somewhere

--- frontend/components/Game.jsx
+++ frontend/components/Game.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Stats } from '@react-three/drei'
 import {Physics, Debug} from '@react-three/cannon';
-import { onPlayerJoin, Joystick } from 'playroomkit';
+import { onPlayerJoin, Joystick, usePlayersList } from 'playroomkit';
 import ReactNipple from 'react-nipple';
 
 
@@ -11,7 +11,7 @@ import Car from "../components/Car.jsx";
 import { Player } from './Lobby.jsx';
 
 function Game() {
-    let players = [];
+    let players = usePlayersList();
 
     // Create a joystick controller for each joining player
     onPlayerJoin((state)=>{
@@ -25,7 +25,7 @@ function Game() {
     })
     
     return (
-        <div>
+        <group>
         <Canvas gl={{ preserveDrawingBuffer: true }} camera={{ position: [0, 3, 15], fov:45, near: 1, far: 1000 }}>
             <color attach="background" args={['#aeccfc']} />
             <ambientLight intensity={0.5} color={"white"}/>
@@ -44,7 +44,7 @@ function Game() {
         onEnd={()=> myPlayer().setState("dir", undefined)}
         onPlain={(e, data)=> 
           myPlayer().setState("dir", data.direction)} />
-          </div>
+        </group>
     )
 }
 
